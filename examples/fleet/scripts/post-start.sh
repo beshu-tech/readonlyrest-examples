2a9be93aa7887f79a0a7ea232f88211441525c4e
Mateusz Kołodziejczyk <coutopl@gmail.com>
2026-04-16 11:02:10 +0200

    wip

--- examples/fleet/scripts/post-start.sh
+++ examples/fleet/scripts/post-start.sh
@@ -3,7 +3,5 @@ echo -e ""
 echo -e "Users:"
 echo -e "  admin:admin"
 echo -e ""
-echo -e "Fleet Server:  https://localhost:8220"
-echo -e "APM Server:    http://localhost:8201"
 echo -e "Demo App:      http://localhost:3001"
 open https://localhost:15601
